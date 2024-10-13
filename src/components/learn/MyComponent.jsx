9076b71aeddffb2880fcaa1ef14432abc8c2de7e
Quyen <nguyenhuuquyen233@gmail.com>
2024-10-13 19:08:46 +0700

    video 33

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -3,8 +3,10 @@
 import './style.css'
   
 const MyComponent = () => {
+  const cua = "1 2 3 con tho"
     return (
       <>
+      <div> {JSON.stringify(cua)} co dep trai ko </div>
       <div> Cua handsome </div>
       <div className='thenao'> vai ca l </div>
       </>
