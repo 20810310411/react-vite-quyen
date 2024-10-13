75869f7c17b58eb0287a42dd6396537a601be9c9
Quyen <nguyenhuuquyen233@gmail.com>
2024-10-13 18:14:09 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,7 +1,13 @@
 // () => { } 
+
+import './style.css'
+  
 const MyComponent = () => {
     return (
+      <>
       <div> Cua handsome </div>
+      <div className='thenao'> vai ca l </div>
+      </>
     );
   }
 
