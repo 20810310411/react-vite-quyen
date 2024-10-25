b20628cf76eaf8ef03c718206a8572291899772c
Quyen <nguyenhuuquyen233@gmail.com>
2024-10-25 18:58:21 +0700

    video 47

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,52 @@
-import './components/todo/todo.css'
-import TodoData from './components/todo/TodoData'
-import TodoNew from './components/todo/TodoNew'
-import reactLogo from './assets/react.svg'
-import { useState } from 'react'
-
+import "./components/todo/todo.css";
+import TodoData from "./components/todo/TodoData";
+import TodoNew from "./components/todo/TodoNew";
+import reactLogo from "./assets/react.svg";
+import { useState } from "react";
+import Header from "./components/layout/header";
+import Footer from "./components/layout/footer";
 
 function App() {
-
   const [todoList, setToDoList] = useState([
     // {id: 1, name: "Learning React"},
     // {id: 2, name: "Watching Youtube"}
-  ])
+  ]);
 
-  
   const addNewTodo = (name) => {
     const newTodo = {
       id: randomIntFromInterval(1, 1000000),
-      name: name 
-    }
-    setToDoList([...todoList, newTodo])
-  }
+      name: name,
+    };
+    setToDoList([...todoList, newTodo]);
+  };
 
-  const randomIntFromInterval = (min,max) => {
+  const randomIntFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
-  }
+  };
 
   const deleteTodo = (id) => {
-    const newTodo = todoList.filter(item => item.id !== id)
-    setToDoList(newTodo)
-  }
+    const newTodo = todoList.filter((item) => item.id !== id);
+    setToDoList(newTodo);
+  };
 
   return (
-    <div className="todo-container">
-      <div className="todo-title"> Todo List</div>
-      <TodoNew
-        addNewTodo={addNewTodo}
-      />
-      
-      {
-        todoList.length > 0 ?
-        <TodoData
-        todoList = {todoList}
-        deleteTodo = {deleteTodo}
-      />
-      :
-        <div className='todo-img'>
-        <img src={reactLogo} className='logo'/>
+    <>
+      <Header />
+      <div className="todo-container">
+        <div className="todo-title"> Todo List</div>
+        <TodoNew addNewTodo={addNewTodo} />
+
+        {todoList.length > 0 ? (
+          <TodoData todoList={todoList} deleteTodo={deleteTodo} />
+        ) : (
+          <div className="todo-img">
+            <img src={reactLogo} className="logo" />
+          </div>
+        )}
       </div>
-      }
-      
-    </div>
-  )
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
