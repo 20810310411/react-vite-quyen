6df732a5708278ed7f7091e07dc3e9746ec22aa4
Quyen <nguyenhuuquyen233@gmail.com>
2024-11-12 18:41:09 +0700

    video 85 phan trang

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -27,7 +27,7 @@ const DeleteUserAPI = (id) => {
 }
 
 const FetchAllUserAPI = () => {
-    const URL_BACKEND = "/api/v1/user"
+    const URL_BACKEND = "/api/v1/user?current=1&pageSize=1"
     return axios.get(URL_BACKEND)
 }
 
