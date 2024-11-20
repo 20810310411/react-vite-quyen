acda77e90e0a45e746da9546fb28738ad027b8f1
Quyen <nguyenhuuquyen233@gmail.com>
2024-11-20 15:34:29 +0700

    video 98 react context

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -6,10 +6,15 @@ import {
   SettingOutlined,
 } from "@ant-design/icons";
 import { Menu } from "antd";
-import { Children, useState } from "react";
+import { Children, useContext, useState } from "react";
+import { AuthContext } from "../context/auth.context";
 
 const Header = () => {
   const [current, setCurrent] = useState("");
+
+  const {user} = useContext(AuthContext)
+  console.log("check user", user)
+
   const onClick = (e) => {
     console.log("click ", e);
     setCurrent(e.key);
@@ -17,17 +22,17 @@ const Header = () => {
 
   const items = [
     {
-      label: <Link to = {"/"}>Home</Link> ,
+      label: <Link to={"/"}>Home</Link>,
       key: "home",
       icon: <HomeOutlined />,
     },
     {
-      label: <Link to = {"/users"}>Users</Link>,
+      label: <Link to={"/users"}>Users</Link>,
       key: "users",
       icon: <UsergroupAddOutlined />,
     },
     {
-      label: <Link to = {"/books"}>Books</Link>,
+      label: <Link to={"/books"}>Books</Link>,
       key: "books",
       icon: <BookOutlined />,
     },
@@ -35,16 +40,16 @@ const Header = () => {
       label: "Cài đặt",
       key: "settings",
       icon: <SettingOutlined />,
-      Children: [
+      children: [
         {
-          label: <Link to = {"/login"}>Đăng nhập</Link>,
-          key: "login"
+          label: <Link to={"/login"}>Đăng nhập</Link>,
+          key: "login",
         },
         {
           label: "Đăng xuất",
-          key: "logout"
+          key: "logout",
         },
-      ]
+      ],
     },
   ];
   return (
